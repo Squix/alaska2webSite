9c1b34bdb52c07164ac9fe40b41042d7fe4d91d4
Squix <squixnetwork@gmail.com>
2020-08-26 15:02:37 +0200

    Mise en place du système de requête de la base de données

--- server/controllers/sorter.controller.js
+++ server/controllers/sorter.controller.js
@@ -1,7 +1,24 @@
 
+const csvdb = require("node-csv-query").default;
+const path = require("path");
+
+let db = null;
+
 class SorterController {
-    static sort = (req,res) => {
+    static sort = async (req,res) => {
         console.log(req.params)
+        //on se "connecte" au fichier csv
+        db = await csvdb(process.cwd()+'/server/public/'+process.env.DB_FILE_NAME,
+        {
+            delimiter:';',
+            quote:false,
+            relax:true,
+            
+        })
+        let records = await db.find({
+            "2-Manufacturer":"Google"
+        })
+        res.json(records)
     }
 }
 
