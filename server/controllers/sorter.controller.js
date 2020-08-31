75708426f22b57ea67577c6cc9091608e07ad3e6
Squix <squixnetwork@gmail.com>
2020-08-31 18:15:03 +0200

    Correction du code pour gérer correction les plages de données

--- server/controllers/sorter.controller.js
+++ server/controllers/sorter.controller.js
@@ -11,21 +11,21 @@ class SorterController {
     static critera_list = [
         ["manufacturer","2-Manufacturer"],
         ["camera_model","3-Camera Model"],
-        ["year","4-Year"],
+        ["year","4-Year","range"],
         ["camera_type","5-Camera Type"],
-        ["fnumber", "6-F-number"],
-        ["iso","7-ISO"],
+        ["fnumber", "6-F-number","range"],
+        ["iso","7-ISO","range"],
         ["aperture","8-Aperture"],
-        ["exposure","9-Exposure"],
-        ["mega_pixels","10-MegaPixels"],
-        ["raw_size","11-RAWsize"],
-        ["focal_length","12-Focallength"],
-        ["focal_length_eq35","13-Focallength Eq. 35Mm"],
-        ["crop_factor","14-cropFactor"],
+        ["exposure","9-Exposure","range"],
+        ["mega_pixels","10-MegaPixels","range"],
+        ["raw_size","11-RAWsize","range"],
+        ["focal_length","12-Focallength","range"],
+        ["focal_length_eq35","13-Focallength Eq. 35Mm","range"],
+        ["crop_factor","14-cropFactor","range"],
         ["sensor_size","15-Sensor size"],
-        ["sensor_size_mm","16-Sensor Size (mm)"],
-        ["pixel_pitch","17-pixel pitch (um)"],
-        ["pixel_density","18-Pixel Density (MP/cm2)"],
+        ["sensor_size_mm","16-Sensor Size (mm)","range"],
+        ["pixel_pitch","17-pixel pitch (um)","range"],
+        ["pixel_density","18-Pixel Density (MP/cm2)","range"],
         ["sensor_model","19-Sensor Model"],
         ["demosaicing_algorithm","20-Demosaicing algorithm"],
         ["usm|den_order","21-USM / DEN order"],
@@ -33,7 +33,7 @@ class SorterController {
         ["denoising","23-Denoising"],
         ["resizing","24-Resizing"],
         ["image_size","25-ImageSize"],
-        ["jpeg_compression","26-JPEG Compression"]
+        ["jpeg_compression","26-JPEG Compression","range"]
 
     ]
 
@@ -47,7 +47,13 @@ class SorterController {
             //si le critère est dans la requête
             if(req.query[criteria[0]]) {
                 //on l'ajoute au filtre
-                criteriaObject[criteria[1]] = req.query[criteria[0]]
+                criteriaObject[criteria[1]] = {
+                    supplied_value:req.query[criteria[0]]
+                }
+                //si c'est un InputRange, on le précise
+                if(criteria[2] === "range") {
+                    criteriaObject[criteria[1]].type = "range"
+                }
             }
         }
 
@@ -67,11 +73,23 @@ class SorterController {
             let matchingRecord = true;
             //pour chaque élément du filtre, on vérifie si le record contient un élément de la liste des critères
             for (const criteria in criteriaObject) {
-                const value = criteriaObject[criteria]
-                //console.log("value array", value.split(',').indexOf(record[criteria]))
-                if(value.split(',').indexOf(record[criteria]) === -1) {
+                const value = criteriaObject[criteria].supplied_value
+                
+                //si c'est un critère DynamicSelect
+                if( criteriaObject[criteria].type !== "range" && value.split(',').indexOf(record[criteria]) === -1) {
                     //si l'un des critères ne correspond pas, on s'en va
                     matchingRecord = false;
+                }
+                //si c'est un inputRange
+                else if(criteriaObject[criteria].type === "range") {
+                    //si la valeur du record n'est pas dans la plage donnée
+                    let range = {
+                        min:parseFloat(value.split(',')[0]),
+                        max:parseFloat(value.split(',')[1])
+                    }
+                    if(parseFloat(record[criteria]) < range.min || parseFloat(record[criteria]) > range.max) {
+                        matchingRecord = false;     
+                    }
                 }
             }
             return matchingRecord
