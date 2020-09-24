541ca01d4858cc56c29e06b98ff93faf1872ebc0
Squix <squixdev.squix@gmail.com>
2020-09-24 12:24:03 +0200

    Système pour désactiver l'HTTPS et build final

--- app.js
+++ app.js
@@ -21,17 +21,6 @@ app.use('/TRAINING_RAW', express.static( path.join(__dirname, 'TRAINING_RAW') ))
 var https = require('https');
 var http = require('http');
 
-//var fs = require('fs');
-
-var options = {
-  key: fs.readFileSync('../key/alaska.utt.fr.key', 'utf8'),
-  cert: fs.readFileSync('../key/alaska_utt_fr.crt', 'utf8'),
-  ca: fs.readFileSync('../key/DigiCertCA.crt', 'utf8'), 
-  rejectUnauthorized: false,
-  requestCert: true
-};
-
-
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
@@ -51,48 +40,39 @@ httpServer.on('request', function(req, res) {
     
 });
 
-var httpsServer=https.createServer(options, app)
-
-httpsServer.on('request', function(req, res) {
-    console.log('HTTPS REQUEST');
-    console.log(req.url);
-    console.log(req.method);
-//    console.log(req.headers);
-});
-
-
-/*var PS=https.createServer(options, app).listen(
-  process.env.PORT, 
-  console.log(`Server listening on port ${process.env.PORT}`)
-)*/
-/*
-httpsServer.on('request', function(request, response) {
-    console.log(request.url);
-});
-*/
-
-//var morgan = require('morgan');
-//var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
-//app.use(morgan('combined', {stream: accessLogStream}))
-
-// run server
-/*
-https.createServer(options, app).listen(
-  process.env.PORT, 
-  console.log(`Server listening on port ${process.env.PORT}`)
-)
-*/
-/*app.listen(
-  process.env.PORT,
-  console.log(`Server listening on port ${process.env.PORT}`)
-)*/
 app.set('httpsPort', process.env.PORTHTTPS);
 app.set('httpPort', process.env.PORTHTTP);
 
-httpsServer.listen(
-  process.env.PORTHTTPS,
-  console.log(`Server listening on port ${process.env.PORTHTTPS}`)
-)
+if(JSON.parse(process.env.ENABLE_HTTPS)) {
+
+  var httpsServer=https.createServer({
+    key: fs.readFileSync('../key/alaska.utt.fr.key', 'utf8'),
+    cert: fs.readFileSync('../key/alaska_utt_fr.crt', 'utf8'),
+    ca: fs.readFileSync('../key/DigiCertCA.crt', 'utf8'), 
+    rejectUnauthorized: false,
+    requestCert: true
+  }, app)
+
+  httpsServer.on('request', function(req, res) {
+      console.log('HTTPS REQUEST');
+      console.log(req.url);
+      console.log(req.method);
+  //    console.log(req.headers);
+  });
+  
+  httpsServer.listen(
+    process.env.PORTHTTPS,
+    
+    () => {
+      
+      console.log("HTTPS enabled.")
+      console.log(`Server listening on port ${process.env.PORTHTTPS}`)
+    }
+  )
+
+}
+
+
 httpServer.listen(
   process.env.PORTHTTP,
   console.log(`Server listening on port ${process.env.PORTHTTP}`)
